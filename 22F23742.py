02d64bb04828e55b216d5a2d4e16515bedf1e42b
alialajmi-6769 <alialajmi783@gmail.com>
2026-06-02 18:48:26 +0400

    Create 22F23742.py

---
+++ 22F23742.py
@@ -0,0 +1,27 @@
+def tourist_density(location_type, current_visitors, capacity):
+
+    if capacity <= 0:
+        return "Invalid capacity"
+
+    occupancy_rate = (current_visitors / capacity) * 100
+
+    match location_type:
+        case 1:
+            location_name = "Beach"
+        case 2:
+            location_name = "Museum"
+        case 3:
+            location_name = "Historic Site"
+        case _:
+            location_name = "Unknown"
+
+    if occupancy_rate < 40:
+        density_status = "Low"
+    elif occupancy_rate < 70:
+        density_status = "Moderate"
+    elif occupancy_rate < 90:
+        density_status = "High"
+    else:
+        density_status = "Critical"
+
+    return location_name, round(occupancy_rate, 2), density_status
